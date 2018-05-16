653cc9b5f0b72a2de5b09535c6034b7fb52334e5
李新智 <lixinzhi@lixinzhidemacbook-pro.local>
2018-05-16 22:34:42 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 李新智 on 2018/5/16.
+//  Copyright © 2018年 李新智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
